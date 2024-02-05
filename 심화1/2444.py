01cb553cdbd2e64eeb158b4fb43aca5e48204907
SooHwanC <frcp9408@gmail.com>
2024-02-05 17:53:44 +0900

    0205_2444

---
+++ 심화1/2444.py
@@ -0,0 +1,11 @@
+count = int(input())
+num1 = count-1
+for i in range(1, count*2, +2):
+    print(" "*num1, end="")
+    print("*"*i)
+    num1 -= 1
+num1 += 2
+for i in range((count*2) -3, 0, -2):
+    print(" "*num1, end="")
+    print("*"*i)
+    num1 += 1
