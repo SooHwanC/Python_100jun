04f6129804fce39dd236b48fc2409002c9d19e48
SooHwanC <frcp9408@gmail.com>
2024-02-13 16:06:38 +0900

    0213_2563

--- 2차원 배열/2563.py
+++ 2차원 배열/2563.py
@@ -1,14 +1,8 @@
-arr = [[0 for i in range(100)] for i in range(100)]
-
-# 입력 받기
+arr = [[0]*100 for i in range(100)]
 for i in range(int(input())):
     x, y = map(int, input().split())
     for i in range(x, x + 10):
         for j in range(y, y + 10):
             arr[i][j] = 1
-    
-# 값이 1인 것의 수 세기
-count = sum(sum(row) for row in arr)
-
-# 결과 출력
+count = sum(sum(i) for i in arr)
 print(count)
